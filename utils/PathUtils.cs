35f342b10d6b41e989461b3d4d627db07b173241
MrKelpy <alexandresilva.coding@gmail.com>
2023-04-17 23:29:40 +0100

    Added the PathUtils class, which should've been included in v1.2

---
+++ utils/PathUtils.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace PgpsUtilsAEFC.utils
+{
+    /// <summary>
+    /// This class aims to provide a set of utilities to work with paths.
+    /// </summary>
+    public static class PathUtils
+    {
+        
+        /// <summary>
+        /// Normalizes a path, changing it to a common format.
+        /// Every path should be formatted as such.: "path/to/directory"
+        /// </summary>
+        /// <param name="path">The path to normalize.</param>
+        /// <returns>The normalized path.</returns>
+        public static string NormalizePath(string path)
+        {
+            path = path.TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+            path = path.Replace("\\", @"/");
+            path = path.Replace(@"\", @"/");
+            path = path.Replace(@"/./", @"/");
+            return path;
+        }
+    }
+}
